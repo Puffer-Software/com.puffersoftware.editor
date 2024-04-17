afa7037f66d393134ed51a718b345fed760d1e95
onurozelpf <167096567+onurozelpf@users.noreply.github.com>
2024-04-17 14:54:56 +0300

    Add files via upload

---
+++ Runtime/ClearAllPlayerPrefsToolWindow.cs
@@ -0,0 +1,19 @@
+#if UNITY_EDITOR
+using PufferSoftware.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace PufferSoftware.Editor
+{
+    public class ClearAllPlayerPrefsToolWindow : MonoBehaviour
+    {
+        [MenuItem("Puffer Software/Clear All Player Prefs", false, 1)]
+        public static void ClearAllPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            PSDebug.Log("<color=orange> <color=green>Success : </color>All Player Prefs Cleared!</color>");
+        }
+    }
+}
+
+#endif
